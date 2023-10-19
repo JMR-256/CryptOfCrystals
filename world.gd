74fd53c90094733e1b46965f5b67cf24ee9f0d4d
JMR-256 <james-sec@protonmail.com>
2023-10-19 22:56:27 +0100

    feat: added timed popup when user gets gem and log win condition
    To check if the user has the total points the world has to be coupled to
    the spawner. This could probably be refactored, though for this project,
    it makes sense for the coupling anyway

--- world.gd
+++ world.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+signal score_updated()
+@onready var winningPoints = $gemspawner.num_of_gems
 var points:int = 0
 
 func _unhandled_input(_event):
@@ -8,5 +10,7 @@ func _unhandled_input(_event):
 
 func _on_gemspawner_gem_collected():
 	points += 1
-	print(points)
-	
+	print("Collected gem. Total gems: ", points)
+	if(points == winningPoints):
+		print("Win condition")
+	score_updated.emit(points)
